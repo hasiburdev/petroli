63bf3d95c5391148de11bfe5a41b241cc9f72236
Hasibur Rahman <hasiburdev@gmail.com>
2022-10-18 14:24:29 +0600

    Feat: Add index file for images

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,21 @@
+import logo from './logo.png';
+import banner from './banner.png';
+import service1 from './service-1.png';
+import service2 from './service-2.png';
+import service3 from './service-3.png';
+import supplier1 from './supplier-1.png';
+import supplier2 from './supplier-2.png';
+import supplier3 from './supplier-3.png';
+import supplier4 from './supplier-4.png';
+
+export {
+  logo,
+  banner,
+  service1,
+  service2,
+  service3,
+  supplier1,
+  supplier2,
+  supplier3,
+  supplier4,
+};
